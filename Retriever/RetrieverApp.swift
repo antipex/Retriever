d61033d53e435338003ef38a3eb978411bde3619
Kyle Rohr <public@antipex.com>
2024-12-03 14:50:58 -0800

    Initial Commit

---
+++ Retriever/RetrieverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetrieverApp.swift
+//  Retriever
+//
+//  Created by Kyle Rohr on 3/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RetrieverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RetrieverTests/RetrieverTests.swift
@@ -0,0 +1,16 @@
+//
+//  RetrieverTests.swift
+//  RetrieverTests
+//
+//  Created by Kyle Rohr on 3/12/2024.
+//
+
+import Testing
+
+struct RetrieverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
